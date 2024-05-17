9f05117946a4c022c60d1131c7799f08e3bfa2d3
Brittney Hooker <139884664+Bhooker88@users.noreply.github.com>
2024-05-17 09:45:40 -0400

    Update firebase-config.js
    clean up

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,20 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// Load environment variables only in non-production environments
+
 if (process.env.NODE_ENV !== 'production') {
   const dotenv = require('dotenv');
   dotenv.config();
 }
 
-// Log environment variables to ensure they are loaded
-console.log('API Key:', process.env.REACT_APP_API_KEY);
-console.log('Auth Domain:', process.env.REACT_APP_AUTH_DOMAIN);
-console.log('Project ID:', process.env.REACT_APP_PROJECT_ID);
-console.log('Storage Bucket:', process.env.REACT_APP_STORAGE_BUCKET);
-console.log('Messaging Sender ID:', process.env.REACT_APP_MESSAGING_SENDER_ID);
-console.log('App ID:', process.env.REACT_APP_APP_ID);
-console.log('Measurement ID:', process.env.REACT_APP_MEASUREMENT_ID);
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
